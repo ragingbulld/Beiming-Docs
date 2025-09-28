0139ee6d2802c4445976fbbef87187c99dfc5ff0
ragingbulld <ragingbullniu@gmail.com>
2025-09-28 12:39:29 +0800

    update

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -8,6 +8,7 @@ import { NProgress } from 'nprogress-v2/dist/index.js' // 进度条组件
 import 'nprogress-v2/dist/index.css' // 进度条样式
 import Confetti from "./components/Confetti.vue"
 
+let homePageStyle: HTMLStyleElement | undefined
 export default {
   extends: DefaultTheme,
   Layout: () => {
@@ -16,16 +17,6 @@ export default {
     })
   },
   enhanceApp({ app, router, siteData }) {
-    app.component("Confetti", Confetti); //注册全局组件
+  app.component("Confetti", Confetti); //注册全局组件
   },
-  if (inBrowser) {
-      NProgress.configure({ showSpinner: false })
-      router.onBeforeRouteChange = () => {
-        NProgress.start() // 开始进度条
-      }
-      router.onAfterRouteChanged = () => {
-         busuanzi.fetch()
-         NProgress.done() // 停止进度条
-      }
-}
 }
